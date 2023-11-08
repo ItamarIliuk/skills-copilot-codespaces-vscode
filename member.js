cb1f8dec77a7640ce30d197664c7d805fed23fa0
Itamar Iliuk <itamar.iliuk@gmail.com>
2023-11-08 17:50:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a member of the skills system.
+ * @function
+ * @name skillsMember
+ * @returns {void}
+ */
+function skillsMember(name, surname, email, password) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.password = password;
+}
